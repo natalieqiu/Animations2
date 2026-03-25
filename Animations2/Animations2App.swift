9617b68e9e0bbd1fd4518efd7dd0cabc104c7277
Natalie Qiu <natalieqiu5@gmail.com>
2026-03-25 15:17:56 -0400

    Initial Commit

---
+++ Animations2/Animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations2App.swift
+//  Animations2
+//
+//  Created by Natalie Q on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations2
+//
+//  Created by Natalie Q on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
